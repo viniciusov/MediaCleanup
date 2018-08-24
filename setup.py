c245ab8cd998f04e7e9a36adabf86802a29959a7
Vinícius Orsi Valente <viniciusov@hotmail.com>
2018-08-24 11:20:57 -0300

    To install external libraries

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = 'MediaCleanup',
+    version = '1.4',
+    description = 'Cleanup your Media files and folders',
+    author = 'Vinícius Orsi Valente',
+    author_email = 'viniciusov@hotmail.com',
+    py_modules = ['mediacleanup'],
+    install_requires = ['Send2Trash'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ]
+)
